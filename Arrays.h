7db3922d6bc1ce6d5d40d7fd9ee3c37d934748d1
hasnain1230 <hasnain1230@gmail.com>
2022-01-27 17:59:50 -0500

    Initial commit

---
+++ Arrays.h
@@ -0,0 +1,8 @@
+#ifndef CTESTPROJECT_ARRAYS_H
+#define CTESTPROJECT_ARRAYS_H
+
+#include <stdio.h>
+
+size_t createArray(size_t dimensions);
+
+#endif //CTESTPROJECT_ARRAYS_H
---
+++ Boolean.h
@@ -0,0 +1,7 @@
+#ifndef _BOOLEANL_H
+#define _BOOLEANL_H
+
+#define bool _Bool
+#define true 1
+#define false 0
+#endif
